90c4a99e67e28f918a2b3611f6c733e5b3fff3d7
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-15 18:31:11 +0530

    Check

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+function viewadd() {
+    const url = "https://poojana-cse.github.io/INTERN-MERN/ADD%20-%20Project/index.html";
+    window.open(url, "_blank");
+}
+function viewbox() {
+    const url = "https://poojana-cse.github.io/INTERN-MERN/ADD%20-%20Project/index.html";
+    window.open(url, "_blank");
+}
+function viewemojii() {
+    const url = "https://poojana-cse.github.io/INTERN-MERN/ADD%20-%20Project/index.html";
+    window.open(url, "_blank");
+}
+function viewapi() {
+    const url = "https://poojana-cse.github.io/INTERN-MERN/ADD%20-%20Project/index.html";
+    window.open(url, "_blank");
+}
