527a9dbd4d88a21300d25096728aabf54e1e2072
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-20 07:38:17 +0530

    Completed

--- tools.js
+++ tools.js
@@ -1,6 +1,6 @@
-const xvalue=['C','C++','JAVA','Python'];
-const yvalue=[80,80,70,50];
+const xvalue=['Git','GitHub','Canva','VSCode'];
+const yvalue=[70,80,90,85];
 const barcolor = ["rgb(162, 213, 242)","rgb(211, 249, 216)","rgb(255, 204, 203)","rgb(255, 235, 153)"];
-const layout={title:"Tools"}
+const layout={title:"Programming Languages"}
 const data=[{labels:xvalue,values:yvalue,type:'pie',marker:{colors:barcolor},hole:.1}];
-Plotly.newPlot('lang',data,layout);
+Plotly.newPlot('tool',data,layout);
